b388819904eae525d5b3187e7d280d81b18b070b
Dalawer Hossamin Juwel <juwelrana090@gmail.com>
2024-06-21 13:17:47 +0600

    update

---
+++ database/migrations/2024_06_21_052726_create_plans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->increments('id')->autoIncrement();
+            $table->string('name');
+            $table->string('contract');
+            $table->string('price_month');
+            $table->string('price_year');
+            $table->json('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}
